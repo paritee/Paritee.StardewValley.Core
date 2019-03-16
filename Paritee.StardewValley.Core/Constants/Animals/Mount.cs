649b835e48c70d53c7cf3eb83e7876d4eea82201
Paritee <paritee.hi@gmail.com>
2019-03-16 19:51:10 -0400

    Stable to Mount constants

---
+++ Paritee.StardewValley.Core/Constants/Animals/Mount.cs
@@ -0,0 +1,9 @@
+﻿namespace Paritee.StardewValley.Core.Constants.Animals
+{
+    public class Mount : Models.Animal
+    {
+        public static Mount Horse => new Mount("Horse");
+
+        private Mount(string name) : base(name) { }
+    }
+}
--- Paritee.StardewValley.Core/Constants/Animals/Stable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Paritee.StardewValley.Core.Constants.Animals
-{
-    public class Stable : Models.Animal
-    {
-        public static Stable Horse => new Stable("Horse");
-
-        private Stable(string name) : base(name) { }
-    }
-}
